3070298a131d89bf9007c7b26ead62739fe7ab88
林聖翔 <encorej44641@gmail.com>
2016-11-03 13:16:20 +0800

    Perference Slider and Employee Edit View

---
+++ SliderCell.swift
@@ -0,0 +1,24 @@
+//
+//  SliderCell.swift
+//  shifter
+//
+//  Created by mac on 2016/11/1.
+//  Copyright © 2016年 Chlorophyll. All rights reserved.
+//
+
+import UIKit
+
+class SliderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
