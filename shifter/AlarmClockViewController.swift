75e444b9ba82d4560fe12366f902fe40a3e7a72b
frank21649 <frank@gmail.com>
2016-12-18 22:55:02 +0800

    alarm clock fin

--- shifter/AlarmClockViewController.swift
+++ shifter/AlarmClockViewController.swift
@@ -9,9 +9,8 @@ class AlarmClockViewController: UIViewController{
     var currentUID = String()
     var alarmStoragePlace = String()
     var post = [String : String]()
-    var hour = String()
-    
-    
+    var hour = Int()
+    var minute = Int()
     
     //宣告手機應用程式儲存空間
     let defaults = NSUserDefaults.standardUserDefaults()
@@ -25,14 +24,14 @@ class AlarmClockViewController: UIViewController{
     override func viewWillAppear(animated: Bool) {
         self.tabBarController?.tabBar.hidden = true
         
-//        //set default time
-//        let timeLabel:String = "\(hourLabel)" +":" + "\(minuteLabel)"
-//        let dateFormatter = NSDateFormatter()
-//        dateFormatter.dateFormat = "hh:mm"
-//        let time = dateFormatter.dateFromString(timeLabel)
-//        print("ttttttt", timeLabel)
-//        print(time)
-//        datePicker.date = time!
+        //set default time
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+        if defaults.objectForKey("timeString") != nil{
+            let time = dateFormatter.dateFromString(defaults.objectForKey("timeString") as! String)
+            datePicker.date = time!
+        }
+        
         
     }
     
@@ -41,12 +40,26 @@ class AlarmClockViewController: UIViewController{
         self.title = "提醒設定"
         let tabBarVC = self.tabBarController as? TabBarViewController
         currentUID = tabBarVC!.currentUID
+        
+        let databaseRef = FIRDatabase.database().reference()
         alarmStoragePlace = "employee/"+"\(currentUID)"+"/alarmClock"
         
         //UI
         datePicker.setValue(UIColor.whiteColor(), forKeyPath: "textColor")
         datePicker.datePickerMode = .CountDownTimer
         
+        //get time from DB
+        databaseRef.child(alarmStoragePlace).observeEventType(.Value, withBlock: {snapshot in
+            let interval = Int(snapshot.value!["interval"] as! String)
+            
+            self.minute = interval!%60
+            self.hour = (interval!-self.minute)/60
+            
+            self.minuteLabel.text = String(self.minute)
+            self.hourLabel.text = String(self.hour)
+            
+            
+        })
         
         self.navigationItem.backBarButtonItem?.tintColor = UIColor.whiteColor()
         if defaults.boolForKey("switchState") == true {
@@ -73,17 +86,31 @@ class AlarmClockViewController: UIViewController{
     }
     
     @IBAction func pickerChange(sender: UIDatePicker) {
-        var dateFormatter1 = NSDateFormatter()
-        var dateFormatter2 = NSDateFormatter()
-        dateFormatter1.dateFormat = "hh"
+        let dateFormatter1 = NSDateFormatter()
+        let dateFormatter2 = NSDateFormatter()
+        let dateFormatter0 = NSDateFormatter()
+        
+        dateFormatter1.dateFormat = "HH"
         dateFormatter2.dateFormat = "mm"
+        dateFormatter0.dateFormat = "HH:mm"
         
-        self.hourLabel.text = dateFormatter1.stringFromDate(sender.date)
-        self.minuteLabel.text = dateFormatter2.stringFromDate(sender.date)
+        let hourString = dateFormatter1.stringFromDate(sender.date)
+        let minuteString = dateFormatter2.stringFromDate(sender.date)
+        let timeString = dateFormatter0.stringFromDate(sender.date)
         
-//        let databaseRef = FIRDatabase.database().reference()
-//        let post = ["interval" : String(minutes)]
-//        databaseRef.child(alarmStoragePlace).updateChildValues(post)
+        defaults.setObject(timeString, forKey: "timeString")
+        
+        
+        let hour = Int(hourString)
+        let minute = Int(minuteString)
+        
+        let time = String(hour!*60 + minute!)
+        
+        let timeStruct = ["interval" : time]
+        
+        let databaseRef = FIRDatabase.database().reference()
+        databaseRef.child(alarmStoragePlace).updateChildValues(timeStruct)
+        
     }
     
 }
